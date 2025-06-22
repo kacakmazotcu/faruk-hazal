16c43f7cd4ed6b615692b72d5b5f808530f9a57f
kacakmazotcu <frk.ssm52@gmail.com>
2025-06-22 16:31:25 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Main JS loaded');
+
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.getElementById("surpriseButton");
+    const audio = document.getElementById("music");
+    if (btn && audio) {
+        btn.addEventListener("click", function () {
+            if (audio.paused) {
+                audio.play();
+            } else {
+                audio.pause();
+            }
+        });
+    }
+});
